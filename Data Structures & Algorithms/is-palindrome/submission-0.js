2371b4fc3b0b39a61da0d801d99a1c8cdb66a622
lhikarul <711hikaru@gmail.com>
2026-07-06 16:35:03 +0800

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1
+
+        const isAlphaNumeric = (char) => {
+            return /[a-zA-Z0-9]/.test(char);
+        };
+
+        while (left < right) {
+            while (left < right && !isAlphaNumeric(s[left])) {
+                left++
+            }
+            while (left < right && !isAlphaNumeric(s[right])) {
+                right--
+            }
+
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false;
+            }
+
+            left++
+            right--
+        }
+
+        return true
+    }
+}
